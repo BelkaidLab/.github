2d18c1da7820dc1cd105a497b1b72821c6c31b12
Marwen Belkaid <46497986+marwenbelkaid@users.noreply.github.com>
2023-11-24 15:13:52 +0100

    Update README

---
+++ profile/README.d
@@ -0,0 +1,3 @@
+Belkaid Lab is a research team led by Marwen Belkaid @ ETIS Lab.
+
+Here at the Belkaid Lab, we study human brains using robots and design neuro-inspired robot-brains. [More info](https://marwenbelkaid.github.io/)
